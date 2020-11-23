e31b82657fe5ab67a989a1c50e6632cf9c1bd78a
gkulik-6581 <66838666+gkulik-6581@users.noreply.github.com>
2020-11-23 15:18:29 -0500

    Fixed invalid input

--- Odd_Even/Program.cs
+++ Odd_Even/Program.cs
@@ -15,21 +15,21 @@ namespace Odd_Even
             int oddOrEven = (userInt % 2);
 
 
-            while (oddOrEven == 1 || oddOrEven == 0)
+            while (userInt < 1 || userInt > 1000)
             {
                 //TODO check for other invalid inputs
-                if (userInt < 1 || userInt > 1000)
-                {
-                    Console.WriteLine("Invalid input. Enter a whole number between 1 and 1000.");
-                    userNumber = Console.ReadLine();
-                    oddOrEven = (userInt % 2);
-                }
-                else if (oddOrEven == 1)
-                {
-                    Console.WriteLine($"You entered " + userNumber + ". It is an odd number.");
-                }
-                else Console.WriteLine($"You entered " + userNumber + ". It is an even number.");
+                Console.WriteLine("Invalid input. Enter a whole number between 1 and 1000.");
+                userNumber = Console.ReadLine();
+                userInt = Int32.Parse(userNumber);
+                oddOrEven = (userInt % 2);
             }
+
+            if (oddOrEven == 1)
+            {
+                Console.WriteLine($"You entered " + userNumber + ". It is an odd number.");
+            }
+            else Console.WriteLine($"You entered " + userNumber + ". It is an even number.");
+
         }
     }
 }
