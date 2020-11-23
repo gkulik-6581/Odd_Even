d5bb521ab7cb518dceb62433e077263aff0583ec
gkulik-6581 <66838666+gkulik-6581@users.noreply.github.com>
2020-11-23 15:07:58 -0500

    Trying new way to check for invalid inputs

--- Odd_Even/Program.cs
+++ Odd_Even/Program.cs
@@ -6,7 +6,30 @@ namespace Odd_Even
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Get user input
+            Console.WriteLine("Enter a whole number between 1 and 1000.");
+            string userNumber = Console.ReadLine();
+            //Convert string to int
+            int userInt = Int32.Parse(userNumber);
+
+            int oddOrEven = (userInt % 2);
+
+
+            while (oddOrEven == 1 || oddOrEven == 0)
+            {
+                //TODO check for other invalid inputs
+                if (userInt < 1 || userInt > 1000)
+                {
+                    Console.WriteLine("Invalid input. Enter a whole number between 1 and 1000.");
+                    userNumber = Console.ReadLine();
+                    oddOrEven = (userInt % 2);
+                }
+                else if (oddOrEven == 1)
+                {
+                    Console.WriteLine($"You entered " + userNumber + ". It is an odd number.");
+                }
+                else Console.WriteLine($"You entered " + userNumber + ". It is an even number.");
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+Odd_Even
+
+This short prorgam takes user input asking for a number between 1 and 1000. It tells you if the number is odd or even.
